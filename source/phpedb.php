0b2f825e0703ebfd078aecd966220c6dd4833b53
Max Base <MaxBaseCode@Gmail.Com>
2019-07-10 07:55:37 +0430

    Update phpedb.php

--- source/phpedb.php
+++ source/phpedb.php
@@ -9,4 +9,32 @@
  * @Repository : https://github.com/BaseMax/PHPEDB
  *
  **/
- 
+date_default_timezone_set('America/Los_Angeles');
+function error_page($error)
+{
+	$style="";
+    // $style="
+    // <style>
+    // @font-face
+    // {
+    //   font-family:'site1';
+    //   src:url('fonts/site1.woff') format('woff'),
+    //       url('fonts/site1.ttf') format('truetype');
+    //   font-weight:normal;
+    // }
+    // @font-face
+    // {
+    //   font-family:'site2';
+    //   src:url('fonts/site2.woff') format('woff'),
+    //       url('fonts/site2.ttf') format('truetype');
+    //   font-weight:bold;
+    // }
+    // body
+    // {
+    // 	direction: rtl;
+    // 	font-family:'site1';
+    // }
+    // </style>
+    // ";
+	exit("<meta charset=\"utf-8\">".$style."<br><br><center><h1>Error : ".$error."</h1></center>");
+}
