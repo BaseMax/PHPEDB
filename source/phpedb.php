0d87b5df13684fe35a86496a57bb683e9ea7408f
Max Base <MaxBaseCode@Gmail.Com>
2019-07-10 07:53:21 +0430

    Create phpedb.php

---
+++ source/phpedb.php
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @Name : phpedb.php
+ * @Version : 1.0
+ * @Programmer : Max
+ * @Date : 2016-2019, 2019-07-10
+ * @Released under : https://github.com/BaseMax/PHPEDB/blob/master/LICENSE
+ * @Repository : https://github.com/BaseMax/PHPEDB
+ *
+ **/
+ 
